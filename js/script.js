9a0a4301e30d7f748ed790218e6d18e2dd999a1e
augustoalvessb <augustohenriqueptc@hotmail.com>
2023-01-20 15:52:49 -0300

    Melhorando portfolio

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-window.alert('Este Site ainda está em Desenvolvimento!!');
+window.alert("Este Site ainda está em Desenvolvimento!!");
 
-const firstProject = document.querySelector('.first');
-const secondProject = document.querySelector('.second');
-const thirdProject = document.querySelector('.third');
-const fourthProject = document.querySelector('.fourth');
-const contact = document.querySelector('.contact');
+const firstProject = document.querySelector(".first");
+const secondProject = document.querySelector(".second");
+const thirdProject = document.querySelector(".third");
+const fourthProject = document.querySelector(".fourth");
+const contact = document.querySelector(".contact");
 
-
-
-firstProject.addEventListener('click', () => {
-    window.location.href = './calculadora/index.html';
+firstProject.addEventListener("click", () => {
+  window.location.href = "./calculadora/index.html";
 });
-secondProject.addEventListener('click', () => {
-    window.location.href = './fogo-doom/index.html';
+secondProject.addEventListener("click", () => {
+  window.location.href = "./fogo-doom/index.html";
 });
-thirdProject.addEventListener('click', () => {
-    window.location.href = './codigoDaLiberdade/index.html';
+thirdProject.addEventListener("click", () => {
+  window.location.href = "./codigoDaLiberdade/index.html";
 });
-fourthProject.addEventListener('click', () => {
-    window.location.href = './calculadora-TMB/index.html';
-})
-contact.addEventListener('click', () => {
-    window.open('https://wa.me/5534988170214');
+fourthProject.addEventListener("click", () => {
+  window.location.href = "./calculadora-TMB/index.html";
+});
+contact.addEventListener("click", () => {
+  window.open("https://wa.me/5534988170214");
 });
